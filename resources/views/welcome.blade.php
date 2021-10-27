7ef6714c12af336b03d5eca6645df55c3f752be1
thomaseyaa <61420084+thomaseyaa@users.noreply.github.com>
2021-10-27 12:08:57 +0200

    Edited app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>My Generator</title>
+        <title>Léa Pdf</title>
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -18,7 +18,7 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-sm-6 mt-5">
-                    <h1 class="text-center mb-4">My Generator</h1>
+                    <h1 class="text-center mb-4">Léa Pdf</h1>
 
                     <form method="post" action="/insertFile" enctype="multipart/form-data">
                         @csrf
